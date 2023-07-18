2f0664eae9543ab5962781762e45ec04b25e2055
Beybala Muxtarov <Beybala1>
2023-07-18 14:17:12 +0400

    finished

--- app/models/User.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__ . '/../../core/Model.php';
-
-class User extends Model
-{
-
-}
--- app/routes/api.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-global $router;
-$router->get('/api', 'HomeController@index');
-$router->get('/api/hi', 'HomeController@hi');
